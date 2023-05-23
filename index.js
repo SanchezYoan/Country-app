a845c6d976e75bd67ab3e4e9082c60d06ae9b5e2
SanchezYoan <yoan30470@hotmail.fr>
2023-05-23 11:05:37 +0200

    filter, sort, slice

--- index.js
+++ index.js
@@ -1,37 +1,69 @@
-
 const countriesContainer = document.querySelector(".countries-container");
-let countriesData = []
+let countriesData = [];
+let sortMethod = "maxToMin";
+const btnSort = document.querySelectorAll(".btnSort")
+
 
 async function fetchCountries() {
-    await fetch(`https://restcountries.com/v3.1/all`)
+  await fetch("https://restcountries.com/v3.1/all")
     .then((res) => res.json())
-    .then((data) => countriesData = data)
+    .then((data) => (countriesData = data));
 
-    // console.log(countriesData);
-    countriesDisplay();
-    
-   
+  console.log(countriesData);
+  countriesDisplay();
 }
 
-countriesDisplay = () => {
-    countriesContainer.innertHTML = countriesData.map(
-        (country) => 
-        
-        `
+function countriesDisplay() {
+  countriesContainer.innerHTML = countriesData
+  .filter((country) =>
+  country.translations.fra.common
+    .toLowerCase()
+    .includes(inputSearch.value.toLowerCase())
+  )
+  .sort((a, b) => {
+    if (sortMethod === "maxToMin") {
+      return b.population - a.population;
+    } else if (sortMethod === "minToMax") {
+      return a.population - b.population;
+    } else if (sortMethod === "alpha") {
+      return a.translations.fra.common.localeCompare(
+        b.translations.fra.common
+      );
+    }
+  })
+  .slice(0, inputRange.value)
+  .map(
+    (country) =>
+      `
         <div class="card">
-          <img src=${country.flags.svg} alt="drapeau ${
-        country.translations.fra.common}" > 
-          <h2>${country.translations.fra.common}</h2>
-          <h4>${country.capital}</h4>
-          <p>Population : ${country.population.toLocaleString()}</p>
+        <img src=${country.flags.svg} alt="drapeau ${
+        country.translations.fra.common
+      }" > 
+        <h2>${country.translations.fra.common}</h2>
+        <h4>${country.capital}</h4>
+        <p>Population : ${country.population.toLocaleString()}</p>
         </div>
       `
-    )
+  )
+  .join("");
 }
 
 window.addEventListener("load", fetchCountries);
+inputSearch.addEventListener("input", countriesDisplay);
 
-// 4 - Créer une fonction d'affichage, et paramétrer l'affichage des cartes de chaque pays grace à la méthode MAP
+inputRange.addEventListener("input", (e) => {
+
+    rangeValue.textContent = e.target.value
+    inputRange.value = e.target.value
+    countriesDisplay();
+})
+btnSort.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      sortMethod = e.target.id;
+      countriesDisplay();
+    });
+});
+  
 
 // 5 - Récupérer ce qui est tapé dans l'input et filtrer (avant le map) les données
 // coutry.name.includes(inputSearch.value);
